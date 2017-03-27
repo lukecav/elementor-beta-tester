10b4c032b029654a4b92a0b515bd769a827bbfe9
Luke Cavanagh <lukecav@users.noreply.github.com>
2017-03-27 12:32:24 -0600

    Update plugin wording

--- elementor-beta-tester.php
+++ elementor-beta-tester.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Elementor Beta Tester
  * Plugin URI: https://github.com/lukecav/elementor-beta-tester/
- * Description: Run bleeding edge versions of WooCommerce from our Github repo. This will replace your installed version of WooCommerce with the latest tagged release on Github - use with caution, and not on production sites. You have been warned.
+ * Description: Run bleeding edge versions of Elementor from our Github repo. This will replace your installed version of Elementor with the latest tagged release on Github - use with caution, and not on production sites. You have been warned.
  * Version: 1.0
  * Author: Luke Cavanagh
  * Author URI: https://elementor.com/
